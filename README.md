8f759c88f0dfe6f5e084cd8ae4e9a8a5bf151be5
NGabry <57180079+NGabry@users.noreply.github.com>
2021-12-03 11:26:09 -0500

    Add dataset creator. First ipynb runthrough.

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 ### Utilizing a psychological assesment dataset to find trends in metacognitive abilities
 
 Using mock-data organized in a fashion which resembles results from the Metacognitive Assesment Scale, this repo provides an example of a k-means clustering analysis to identify group trends in metacognitive ability. 
+j
+This example demonstrates how an exploratory analysis could be used to set the ground for further investigation. 
+
+This example dataset includs subjects who have been administered the MAS-A to assess metacognitive ability, as well as had a personality disorder interview to quantify personality disorder traits. 
+
+In this example dataset, we would expect to see two cluster: High Metacog-Low PD individuals (healhty), and Low Metacog-High PD individuals (unhealthy). 
+
+However, given the results, we then realize that further investigation is warranted to discern additinoal psychological factors which contribute to the third identified cluster.
 
 The provided jupyter-notebook contains the full walkhtrough from data cleaning to visualizaitons. 
 
---
+++ create_dataset.py
@@ -0,0 +1,87 @@
+import random
+
+import numpy as np
+import pandas as pd
+
+def randrange_float(start, stop, step):
+    return random.randint(0, int((stop-start) / step)) * step + start
+
+if __name__ == '__main__':
+
+    #Make a high-MC low-PD dataframe
+    MAS_S = []
+    MAS_O = []
+    MAS_D = []
+    MAS_M = []
+    PD_Traits = []
+    for i in range (0,33333):
+        s   = randrange_float(5.5,8,.5)
+        MAS_S.append(s)
+        o   = randrange_float(4,6,.5)
+        MAS_O.append(o)
+        d   = randrange_float(1.5,3,.5)
+        MAS_D.append(d)
+        m   = randrange_float(4.5,8,.5)
+        MAS_M.append(m)
+        pdt = random.randint(0,27)
+        PD_Traits.append(pdt)
+
+    df1 = pd.DataFrame(list(zip(MAS_S, MAS_O, MAS_D, MAS_M, PD_Traits)),
+                      columns = (['MAS_S', 'MAS_O', 'MAS_D', 'MAS_M', 'PD']))
+
+    #Make a low-MC high-PD dataframe
+    MAS_S = []
+    MAS_O = []
+    MAS_D = []
+    MAS_M = []
+    PD_Traits = []
+    for i in range (0,33333):
+        s   = randrange_float(3,5.5,.5)
+        MAS_S.append(s)
+        o   = randrange_float(2,3.5,.5)
+        MAS_O.append(o)
+        d   = randrange_float(0,1.5,.5)
+        MAS_D.append(d)
+        m   = randrange_float(1,4,.5)
+        MAS_M.append(m)
+        pdt = random.randint(23,50)
+        PD_Traits.append(pdt)
+
+    df2 = pd.DataFrame(list(zip(MAS_S, MAS_O, MAS_D, MAS_M, PD_Traits)),
+                      columns = (['MAS_S', 'MAS_O', 'MAS_D', 'MAS_M', 'PD']))
+
+    #Make a high-MC high-PD dataframe
+    MAS_S = []
+    MAS_O = []
+    MAS_D = []
+    MAS_M = []
+    PD_Traits = []
+    for i in range (0,33333):
+        s   = randrange_float(5.5,8,.5)
+        MAS_S.append(s)
+        o   = randrange_float(4,6,.5)
+        MAS_O.append(o)
+        d   = randrange_float(1.5,3,.5)
+        MAS_D.append(d)
+        m   = randrange_float(4.5,8,.5)
+        MAS_M.append(m)
+        pdt = random.randint(23,50)
+        PD_Traits.append(pdt)
+
+    df3 = pd.DataFrame(list(zip(MAS_S, MAS_O, MAS_D, MAS_M, PD_Traits)),
+                      columns = (['MAS_S', 'MAS_O', 'MAS_D', 'MAS_M', 'PD']))
+
+
+    #Concatenage all dataframes
+    full_df = pd.concat([df1,df2,df3], ignore_index=True)
+
+    #Sprinkle in random 999s
+    ids_to_alt = []
+    for i in range (0,156):
+        j = random.randint(0,99999)
+        ids_to_alt.append(j)
+
+    for i in ids_to_alt:
+        full_df.loc[i] = 999
+
+    full_df.to_csv('Metacog_PD.csv',index=False)
